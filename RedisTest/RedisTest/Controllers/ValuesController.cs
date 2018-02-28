4538f101f2b6b5ec763b1c5f596a6f56061b993c
catcherwong <catcher_hwq@outlook.com>
2018-02-28 23:09:33 +0800

    Add Sample

---
+++ RedisTest/RedisTest/Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿namespace RedisTest.Controllers
+{
+    using System;
+    using Microsoft.AspNetCore.Mvc;
+    using EasyCaching.Core;
+    
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly IEasyCachingProvider _provider;
+
+        public ValuesController(IEasyCachingProvider provider)
+        {
+            this._provider = provider;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            var activity = new Activity
+            { 
+                VoteTime = Convert.ToDateTime("2018-2-12 12:11")
+            };
+
+            _provider.Set("key",activity,TimeSpan.FromSeconds(30));
+
+            var time = _provider.Get<Activity>("key");
+
+            return time.Value.VoteTime?.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+        [Serializable]
+        public class Activity
+        {
+            public DateTime? VoteTime { get; set; }
+        }
+    }
+}
---
+++ RedisTest/RedisTest/Startup.cs
@@ -0,0 +1,45 @@
+﻿namespace RedisTest
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using EasyCaching.Redis;
+    using EasyCaching.Serialization.MessagePack;
+    using EasyCaching.Core.Internal;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDefaultRedisCache(option =>
+            {
+                option.Endpoints.Add(new ServerEndPoint("127.0.0.1", 6379));
+                option.Password = "";
+                option.Database = 10;
+            });
+            //put here!
+            services.AddDefaultMessagePackSerializer();
+
+
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
